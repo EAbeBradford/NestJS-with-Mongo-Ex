475d7b935b47d3c641542b85e1d288fa09009242
Mx. Bradford <emma_bradford@milton.com>
2023-01-19 17:51:48 -0500

    made product model using mongoose

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ignoreElements } from 'rxjs';
 import { Product } from './product.model';
+import { InjectModel} from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductService {
 
     private products: Product[] = [];
 
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
+
     insertProduct(title: string, desc: string, price: number) {
         const prodId = Math.random().toString();
-        const newProduct = new Product(prodId, title, desc, price);
+        const newProduct = new this.productModel(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
     }
