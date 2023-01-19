8a103933505accdf7568dfb31d82e19bc2f6e822
Mx. Bradford <emma_bradford@milton.com>
2023-01-19 13:09:05 -0500

    added mongoose to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://bradford:bradford@cluster0.xj5lqs3.mongodb.net/test')],
   controllers: [AppController],
   providers: [AppService],
 })
