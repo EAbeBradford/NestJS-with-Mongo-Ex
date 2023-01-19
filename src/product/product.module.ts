0d77ef65db606c25d33c7419818735c47e9dd31b
Mx. Bradford <emma_bradford@milton.com>
2023-01-19 15:55:59 -0500

    made products schema

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,6 +3,7 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
+  imports:[],
   controllers: [ProductController],
   providers: [ProductService]
 })
